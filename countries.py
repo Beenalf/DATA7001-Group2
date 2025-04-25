e2462cff9fddc9a70c2e10c95c2d0ccfdcad7d43
Ben <sth48624bt@outlook.com>
2025-04-25 18:08:02 +1000

    Added some files with headings

---
+++ countries.py
@@ -0,0 +1,14 @@
+# Group 2, DATA7001 2025
+
+"""
+Contains data about where and when each summer and winter
+Olympic games was held.
+"""
+
+SUMMER_GAMES  = {
+    "year" : "Location",
+}
+
+WINTER_GAMES = {
+    "year" : "location",
+}
---
+++ load_data.py
@@ -0,0 +1,8 @@
+# Group 2, DATA7001 2025
+
+"""
+Loads and filters the data that will be used in the analysis.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
